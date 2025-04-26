67610f5a95ec0ff32701260a1baa8c40bc6f8471
delille <fabrice.delille@embix.fr>
2025-04-26 09:48:28 +0200

    change name to make orientation easier

--- src/model/colors.go
+++ src/model/colors.go
@@ -15,7 +15,7 @@ const (
 )
 const (
 	// Sticker Colors
-	// Front Face
+	// Front Face (x=1) => 2
 	Front_2_0_0 StickerIndex = iota
 	Front_2_0_1
 	Front_2_0_2
@@ -25,56 +25,56 @@ const (
 	Front_2_2_0
 	Front_2_2_1
 	Front_2_2_2
-	// Back Face
-	Back_SW
-	Back_S
-	Back_SE
-	Back_W
-	Back_Center
-	Back_E
-	Back_NW
-	Back_N
-	Back_NE
-	// Up Face
-	Up_SW
-	Up_S
-	Up_SE
-	Up_W
-	Up_Center
-	Up_E
-	Up_NW
-	Up_N
-	Up_NE
-	// Down Face
-	Down_SW
-	Down_S
-	Down_SE
-	Down_W
-	Down_Center
-	Down_E
-	Down_NW
-	Down_N
-	Down_NE
-	// Left Face
-	Left_SW
-	Left_S
-	Left_SE
-	Left_W
-	Left_Center
-	Left_E
-	Left_NW
-	Left_N
-	Left_NE
-	// Right Face
-	Right_SW
-	Right_S
-	Right_SE
-	Right_W
-	Right_Center
-	Right_E
-	Right_NW
-	Right_N
-	Right_NE
+	// Back Face (x=-1) => 0
+	Back_0_0_0
+	Back_0_0_1
+	Back_0_0_2
+	Back_0_1_0
+	Back_0_1_1
+	Back_0_1_2
+	Back_0_2_0
+	Back_0_2_1
+	Back_0_2_2
+	// Up Face (z=1) => 2
+	Up_0_0_2
+	Up_1_0_2
+	Up_2_0_2
+	Up_0_1_2
+	Up_1_1_2
+	Up_2_1_2
+	Up_0_2_2
+	Up_1_2_2
+	Up_2_2_2
+	// Down Face (z=-1) => 0
+	Down_0_0_0
+	Down_1_0_0
+	Down_2_0_0
+	Down_0_1_0
+	Down_1_1_0
+	Down_2_1_0
+	Down_0_2_0
+	Down_1_2_0
+	Down_2_2_0
+	// Left Face (y=-1) => 0
+	Left_0_0_0
+	Left_0_0_1
+	Left_0_0_2
+	Left_1_0_0
+	Left_1_0_1
+	Left_1_0_2
+	Left_2_0_0
+	Left_2_0_1
+	Left_2_0_2
+	// Right Face (y=1) => 2
+	Right_0_2_0
+	Right_0_2_1
+	Right_0_2_2
+	Right_1_2_0
+	Right_1_2_1
+	Right_1_2_2
+	Right_2_2_0
+	Right_2_2_1
+	Right_2_2_2
 )
 
 // ColorNames maps Color constants to their string representations
@@ -101,55 +101,55 @@ var StickerColorName = map[StickerIndex]string{
 	Front_2_2_1: "white_blue",         // row 2, col 1
 	Front_2_2_2: "white_blue_orange",  // row 2, col 2
 	// Back Face
-	Back_NW:     "yellow_red_blue",
-	Back_N:      "yellow_blue",
-	Back_NE:     "yellow_blue_orange",
-	Back_W:      "yellow_red",
-	Back_Center: "yellow",
-	Back_E:      "yellow_orange",
-	Back_SW:     "yellow_red_green",
-	Back_S:      "yellow_green",
-	Back_SE:     "yellow_green_orange",
+	Back_0_2_0: "yellow_red_blue",
+	Back_0_2_1: "yellow_blue",
+	Back_0_2_2: "yellow_blue_orange",
+	Back_0_1_0: "yellow_red",
+	Back_0_1_1: "yellow",
+	Back_0_1_2: "yellow_orange",
+	Back_0_0_0: "yellow_red_green",
+	Back_0_0_1: "yellow_green",
+	Back_0_0_2: "yellow_green_orange",
 	// Up Face
-	Up_NW:     "blue_red_blue",
-	Up_N:      "blue_blue",
-	Up_NE:     "blue_blue_orange",
-	Up_W:      "blue_red",
-	Up_Center: "blue",
-	Up_E:      "blue_orange",
-	Up_SW:     "blue_red_green",
-	Up_S:      "blue_green",
-	Up_SE:     "blue_green_orange",
+	Up_0_2_2: "blue_red_blue",
+	Up_1_2_2: "blue_blue",
+	Up_2_2_2: "blue_blue_orange",
+	Up_0_1_2: "blue_red",
+	Up_1_1_2: "blue",
+	Up_2_1_2: "blue_orange",
+	Up_0_0_2: "blue_red_green",
+	Up_1_0_2: "blue_green",
+	Up_2_0_2: "blue_green_orange",
 	// Down Face
-	Down_NE:     "green_red_blue",
-	Down_N:      "green_blue",
-	Down_NW:     "green_blue_orange",
-	Down_W:      "green_red",
-	Down_Center: "green",
-	Down_E:      "green_orange",
-	Down_SW:     "green_red_green",
-	Down_S:      "green_green",
-	Down_SE:     "green_green_orange",
+	Down_2_2_0: "green_red_blue",
+	Down_1_2_0: "green_blue",
+	Down_0_2_0: "green_blue_orange",
+	Down_0_1_0: "green_red",
+	Down_1_1_0: "green",
+	Down_2_1_0: "green_orange",
+	Down_0_0_0: "green_red_green",
+	Down_1_0_0: "green_green",
+	Down_2_0_0: "green_green_orange",
 	// Left Face
-	Left_NE:     "red_red_blue",
-	Left_N:      "red_blue",
-	Left_NW:     "red_blue_orange",
-	Left_W:      "red_red",
-	Left_Center: "red",
-	Left_E:      "red_orange",
-	Left_SW:     "red_red_green",
-	Left_S:      "red_green",
-	Left_SE:     "red_green_orange",
+	Left_2_0_2: "red_red_blue",
+	Left_2_0_1: "red_blue",
+	Left_2_0_0: "red_blue_orange",
+	Left_1_0_0: "red_red",
+	Left_1_0_1: "red",
+	Left_1_0_2: "red_orange",
+	Left_0_0_0: "red_red_green",
+	Left_0_0_1: "red_green",
+	Left_0_0_2: "red_green_orange",
 	// Right Face
-	Right_NE:     "orange_red_blue",
-	Right_N:      "orange_blue",
-	Right_NW:     "orange_blue_orange",
-	Right_W:      "orange_red",
-	Right_Center: "orange",
-	Right_E:      "orange_orange",
-	Right_SW:     "orange_red_green",
-	Right_S:      "orange_green",
-	Right_SE:     "orange_green_orange",
+	Right_2_2_2: "orange_red_blue",
+	Right_2_2_1: "orange_blue",
+	Right_2_2_0: "orange_blue_orange",
+	Right_1_2_0: "orange_red",
+	Right_1_2_1: "orange",
+	Right_1_2_2: "orange_orange",
+	Right_0_2_0: "orange_red_green",
+	Right_0_2_1: "orange_green",
+	Right_0_2_2: "orange_green_orange",
 }
 
 // ColorNames maps Color constants to their string representations
